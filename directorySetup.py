c350d2b11b1eb5d54e4164ec4a3732df8b1d3467
“pushingLoading” <pavaoduzevic@outlook.com>
2018-11-06 14:42:08 +0100

    update and fix

--- directorySetup.py
+++ directorySetup.py
@@ -3,9 +3,10 @@ import tkinter.filedialog as tkFileDialog
 import tkinter as tk
 
 def showDialog(r):
-    root = tk.Tk()
+    root = r
+    w = tk.Toplevel (r)
     currdir = os.getcwd()
-    tempdir = tkFileDialog.askdirectory(parent=root, initialdir=currdir, title='Please select isaacData directory')
+    tempdir = tkFileDialog.askdirectory(parent=w, initialdir=currdir, title='Please select isaacData directory')
     if len(tempdir) > 0:
-        root = tk.Toplevel()
+        w.withdraw()
         return tempdir
